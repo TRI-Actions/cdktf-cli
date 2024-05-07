a0e4a4a04e4381dd3bffe5f37c8f4c7d27501390
Onur Ozkaynak (Contractor) <onur.ozkaynak.ctr@tri.global>
2024-05-07 10:59:34 +0300

    update script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,9 @@
 
 export CI=1
 
-cd $GITHUB_WORKSPACE/$INPUT_WORKDIR
-
+cd $GITHUB_WORKSPACE
+pipenv install
+cd $INPUT_WORKDIR
 if [ "$INPUT_ACTION" = "plan" ]; then
   cdktf diff --refresh-only --no-color > plan.out
 elif [ "$INPUT_ACTION" = "deploy" ]; then
