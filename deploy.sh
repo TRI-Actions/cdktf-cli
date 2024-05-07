b2b7dffdee166c521be8aeadb076494df9e79e1f
Onur Ozkaynak (Contractor) <onur.ozkaynak.ctr@tri.global>
2024-05-07 15:21:34 +0300

    add pipenv command

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+pipenv install
 cd $1
 cdktf deploy --auto-approve --no-color > deploy.out
--- plan.sh
+++ plan.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+pipenv install
 cd $1
 cdktf diff --refresh-only --no-color > plan.out
