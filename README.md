91d738ade449404049669c998c027b5b80c9c979
Onur Ozkaynak (Contractor) <onur.ozkaynak.ctr@tri.global>
2024-05-07 09:22:08 +0300

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# CDKTF CLI Custom Action
+
+This action will provide a wrapper around cdktf cli to make its use easier within other GitHub Actions workflows.
+
+Initially it supports plan deploy options but other potential options will be added too.
---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export CI=1
+
+cd $GITHUB_WORKSPACE/$INPUT_WORKDIR
+
+if [ "$INPUT_ACTION" = "plan" ]; then
+  cdktf diff --refresh-only --no-color > plan.out
+elif [ "$INPUT_ACTION" = "deploy" ]; then
+  cdktf deploy --auto-approve --no-color > deploy.out
+fi
