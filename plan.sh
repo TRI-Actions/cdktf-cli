fe8a2826cd38090ec4f17cfe3f859f3414f0f0e2
Onur Ozkaynak (Contractor) <onur.ozkaynak.ctr@tri.global>
2024-05-09 08:27:28 +0000

    debug

--- plan.sh
+++ plan.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+echo Installing dependencies
 pipenv install
 cd $1
+echo Running plan in for $1
 cdktf diff --refresh-only --no-color > plan.out
 
 INDEX=$(awk '/Note: Objects have changed/{ print NR; exit }' plan.out)
 
-echo $INDEX
-
 if [[ $INDEX ]]; then
+  echo Drift Detected!
   echo "drift-status=DRIFTED" >> $GITHUB_OUTPUT
 else
+  echo No drift detected!
   echo "drift-status=IN-SYNC" >> $GITHUB_OUTPUT
 fi
 
-echo $GITHUB_OUTPUT
 echo Done!
 
 exit
