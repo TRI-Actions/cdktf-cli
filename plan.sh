9217ab02e5a0395047b7d7414aa24c403c7cbaa9
Onur Ozkaynak (Contractor) <onur.ozkaynak.ctr@tri.global>
2024-05-09 08:22:33 +0000

    debug

--- plan.sh
+++ plan.sh
@@ -4,13 +4,16 @@ cd $1
 cdktf diff --refresh-only --no-color > plan.out
 
 INDEX=$(awk '/Note: Objects have changed/{ print NR; exit }' plan.out)
-# sed -i "1,$((INDEX-1)) d" plan.out
+
 echo $INDEX
-echo "drift-status=DRIFTED" >> $GITHUB_OUTPUT
-# if [[ $INDEX ]]; then
-#   echo "drift-status=DRIFTED" >> $GITHUB_OUTPUT
-# else
-#   echo "drift-status=IN-SYNC" >> $GITHUB_OUTPUT
-# fi
+
+if [[ $INDEX ]]; then
+  echo "drift-status=DRIFTED" >> $GITHUB_OUTPUT
+else
+  echo "drift-status=IN-SYNC" >> $GITHUB_OUTPUT
+fi
+
+echo $GITHUB_OUTPUT
+echo Done!
 
 exit
